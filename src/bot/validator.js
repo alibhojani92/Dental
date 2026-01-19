5a054b32d020e8cdde3fc6bbb047b2abb1c15c63
alibhojani92 <alibhojani92@gmail.com>
2026-01-19 10:00:57 +0530

    Update validator.js

--- src/bot/validator.js
+++ src/bot/validator.js
@@ -1,14 +1,15 @@
+export const CALLBACK_IDS = [
+  "MENU_STUDY",
+  "MENU_TEST",
+  "MENU_PERFORMANCE",
+  "MENU_REVISION",
+  "MENU_SCHEDULE",
+  "MENU_STREAK",
+  "MENU_SETTINGS",
+  "MENU_ADMIN",
+  "MENU_HELP",
+];
+
 export function isValidCallback(data) {
-  const allowed = [
-    "MENU_STUDY",
-    "MENU_TEST",
-    "MENU_PERFORMANCE",
-    "MENU_REVISION",
-    "MENU_SCHEDULE",
-    "MENU_STREAK",
-    "MENU_SETTINGS",
-    "MENU_ADMIN",
-    "MENU_HELP",
-  ];
-  return allowed.includes(data);
+  return CALLBACK_IDS.includes(data);
 }
