2cf8c6ef2b7c8b0b1b6bb460d6c933b952d20a4e
alibhojani92 <alibhojani92@gmail.com>
2026-01-20 15:54:15 +0530

    Update start.handler.js

--- src/handlers/start.handler.js
+++ src/handlers/start.handler.js
@@ -1,20 +1,17 @@
+// src/handlers/start.handler.js
+
 import { sendMessage } from "../services/telegram.service.js";
 import { MAIN_MENU_KEYBOARD } from "../ui/keyboards.js";
 import { MESSAGES } from "../ui/messages.js";
 
 /**
- * Show main start menu
- * Called only from /start command
+ * Named export (IMPORTANT)
  */
-export async function showStartMenu(chatId, env) {
-  try {
-    await sendMessage(
-      chatId,
-      MESSAGES.START_WELCOME,
-      env,
-      MAIN_MENU_KEYBOARD
-    );
-  } catch (err) {
-    console.error("START HANDLER ERROR:", err);
-  }
+export async function startHandler(chatId, env) {
+  await sendMessage(
+    chatId,
+    MESSAGES.START_WELCOME,
+    env,
+    MAIN_MENU_KEYBOARD
+  );
 }
